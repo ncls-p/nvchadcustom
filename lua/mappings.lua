941650e94ae48eeb99c00a555f166144569285b9
ncls-p <nclspierrot@gmail.com>
2025-04-28 16:42:14 +0200

    Add keybindings for Molten plugin in mappings.lua
    Signed-off-by: ncls-p <nclspierrot@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -18,3 +18,11 @@ local function toggle_copilot()
 end
 
 map("n", "<leader>ce", toggle_copilot, { desc = "Toggle Copilot" })
+
+-- Molten plugin keybindings
+vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
+vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "run operator selection" })
+vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
+vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "evaluate visual selection" })
+
