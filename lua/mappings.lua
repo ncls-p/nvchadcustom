884d895dd429ff9ea3a3657f21d31af982383477
ncls-p <nclspierrot@gmail.com>
2025-04-28 18:46:17 +0200

    feat: add copilot-lsp plugin with key mappings and configuration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 require "nvchad.mappings"
 
 local map = vim.keymap.set
@@ -8,8 +9,23 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
--- Copilot mapping
-map("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false, desc = "Copilot Accept" })
+-- Basic Copilot.vim accept on Ctrl-l
+map("i", "<C-l>", 'copilot#Accept("<CR>")',
+  { silent = true, expr = true, replace_keycodes = false, desc = "Copilot.vim Accept" })
+
+-- Copilot-LSP accept on Ctrl-Shift-l
+map("i", "<C-S-l>", function()
+  local nes = require("copilot-lsp.nes")
+  return nes.walk_cursor_start_edit()
+      or (nes.apply_pending_nes() and nes.walk_cursor_end_edit())
+end, { expr = true, silent = true, desc = "Copilot LSP Accept" })
+
+map("i", "<C-n>", function() return require("copilot#Next")("") end,
+  { expr = true, silent = true, desc = "Copilot Next" })
+map("i", "<C-p>", function() return require("copilot#Previous")("") end,
+  { expr = true, silent = true, desc = "Copilot Previous" })
+map("i", "<C-d>", function() return require("copilot#Dismiss")("") end,
+  { expr = true, silent = true, desc = "Copilot Dismiss" })
 
 -- Toggle Copilot suggestions
 local function toggle_copilot()
@@ -24,5 +40,5 @@ vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc 
 vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "run operator selection" })
 vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
 vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
-vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "evaluate visual selection" })
-
+vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+  { silent = true, desc = "evaluate visual selection" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,6 +28,22 @@ return {
     end
   },
   {
+    "copilotlsp-nvim/copilot-lsp",
+    lazy = false,
+      init = function()
+          vim.g.copilot_nes_debounce = 200
+          vim.lsp.enable("copilot_ls")
+          vim.keymap.set("n", "<tab>", function()
+            -- Try to jump to the start of the suggestion edit.
+              -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+              local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+                  or (
+                      require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit()
+                  )
+          end)
+      end,
+    },
+    {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
